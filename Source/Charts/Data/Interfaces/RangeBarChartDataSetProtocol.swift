b54e8e45677db16c4cf9710a708d1d12f66475ee
race <racechao@gmail.com>
2022-04-05 21:44:48 +0800

    fix build error

--- Source/Charts/Data/Interfaces/RangeBarChartDataSetProtocol.swift
+++ Source/Charts/Data/Interfaces/RangeBarChartDataSetProtocol.swift
@@ -11,14 +11,6 @@ import CoreGraphics
 @objc
 public protocol RangeBarChartDataSetProtocol: LineScatterCandleRadarChartDataSetProtocol
 {
-    // MARK: - Data functions and accessors
-    
-    // MARK: - Styling functions and accessors
-    
-    /// the space that is left out on the left and right side of each candle,
-    /// **default**: 0.1 (10%), max 0.45, min 0.0
-    var barSpace: CGFloat { get set }
-    
     
     /// the width of the bar in pixels.
     ///
